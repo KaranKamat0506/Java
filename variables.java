7f5345f33398a65387e3b3055fb69a36b04b13ce
KaranKamat0506 <karankamat2000@gmail.com>
2022-04-18 22:09:54 +0530

    Variables in Java

---
+++ variables.java
@@ -0,0 +1,36 @@
+public class variables {
+    public static void main(String[] args) {
+        /*
+            Variables in Java: String, int, char, float, boolean 
+            Syntax: Datatype Variable_name = value
+        */
+        // Integer Datatype
+        int a=5;
+        int b=5;
+        System.out.println(a+b);
+
+        // Multiple Variable Declared
+        int x=6,y=7,z=8;
+        z=x+y+z;
+        System.out.println(z);
+
+        //String datatype 
+        String name = "Micheal";
+        System.out.println(name);
+
+        // Float Datatype
+        float amount = 17.7f;
+        System.out.println(amount);
+
+        //Char Datatype
+        char c ='a';
+        System.out.println(c);
+
+        //Boolean Datatype
+        boolean isnull = true;
+        System.out.println(isnull);
+
+
+
+    }
+}
